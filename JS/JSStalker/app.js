96e66e6c78008fb946fd04a8be5ecbec4c255935
Cook1619 <cook6348@gmail.com>
2018-10-25 13:46:36 -0500

    Starting JS

---
+++ JS/JSStalker/app.js
@@ -0,0 +1,9 @@
+// let firstName = prompt('What is your first name?');
+// let lastName =  prompt('What is your last name?');
+// let age = prompt('What is your age?');
+// console.log(`Your full name is ${firstName} ${lastName}`);
+// console.log(`You are ${age} years old.`)
+
+let age = prompt('What is your age?')
+
+alert(`You've been alive for ${age * 365.25} day's`);
