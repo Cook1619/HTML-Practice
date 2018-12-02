061b97147df2b99aa9693a64025c48039a8cf121
Matt <cook6348@gmail.com>
2018-12-02 10:04:56 -0600

    set up keypress events for associated letters

--- Projects/Drum-Kit-Project/index.js
+++ Projects/Drum-Kit-Project/index.js
@@ -1,40 +1,49 @@
 let button = document.querySelectorAll('.drum');
-console.log(button);
+
 for (let i = 0; i < button.length; i++) {
     button[i].addEventListener('click', function () {
         let buttonHTML = this.innerHTML;
-        switch (buttonHTML) {
-            case "w":
-                let audio = new Audio('sounds/tom-1.mp3');
-                audio.play();
-                break;
-            case "a":
-                let audio = new Audio('sounds/tom-2.mp3');
-                audio.play();
-                break;
-            case "s":
-                let audio = new Audio('sounds/tom-3.mp3');
-                audio.play();
-                break;
-            case "d":
-                let audio = new Audio('sounds/tom-4.mp3');
-                audio.play();
-                break;
-            case "j":
-                let audio = new Audio('sounds/kick-bass.mp3');
-                audio.play();
-                break;
-            case "k":
-                let audio = new Audio('sounds/crash.mp3');
-                audio.play();
-                break;
-            case "l":
-                let audio = new Audio('sounds/snare.mp3');
-                audio.play();
-                break;
-
-            default:
-                console.log(buttonHTML);
-        }
+        makeSound(buttonHTML)
     })
 }
+document.addEventListener("keydown", function () {
+    makeSound(event.key);
+});
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            let tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case "a":
+            let tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case "s":
+            let tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case "d":
+            let tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+        case "j":
+            let kickbass = new Audio('sounds/kick-bass.mp3');
+            kickbass.play();
+            break;
+        case "k":
+            let crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case "l":
+            let snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+
+        default:
+            console.log(buttonHTML);
+    }
+}
+
+
