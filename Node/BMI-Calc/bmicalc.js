665d014602addc204cabe6348a06f3b906494a97
Matt <cook6348@gmail.com>
2018-12-04 12:23:26 -0600

    BMI calc with node

---
+++ Node/BMI-Calc/bmicalc.js
@@ -0,0 +1,21 @@
+const express =  require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req, res){
+    let height = parseFloat(req.body.height);
+    let weight =  parseFloat(req.body.weight);
+    let result = weight / (height * height);
+    res.send(`Your BMI is ${result}`);
+})
+
+app.listen(3000, function(){
+    console.log('Server started on port 3000');
+})
