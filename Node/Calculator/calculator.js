7858041a0ea7539dbff805ae49f5b92571e12fc0
Matt <cook6348@gmail.com>
2018-12-04 18:33:33 -0600

    More routes added, and html doc styled

--- Node/Calculator/calculator.js
+++ Node/Calculator/calculator.js
@@ -8,11 +8,22 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 })
 
+app.get("/bmiCalculator", function(req,res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+})
+
 app.post('/', function (req, res) {
     let num1 = Number(req.body.num1);
     let num2 = Number(req.body.num2);
     let result = num1 + num2;
     res.send(`The result of the calculation is ${result}`);
+})
+
+app.post("/bmiCalculator", function(req,res){
+    let weight = parseFloat(req.body.weight);
+    let height = parseFloat(req.body.height);
+    let result = weight / (height * height);
+    res.send(`Your calculated BMI is ${result}`);
 })
 
 app.listen(3000, function () {
--- Node/expressApp/server.js
+++ Node/expressApp/server.js
@@ -2,10 +2,22 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", function(req, res){
-    res.send('Hello');
+app.get("/", function (req, res) {
+    res.send("<h1>Hello World</h1>");
 })
 
-app.listen(3000, function(){
+app.get("/contact", function (req, res) {
+    res.send("Contact me at 123@gmail.com");
+})
+
+app.get("/about", function(req,res){
+    res.send("<h2>Matt Cook</h2> <h3>Position: Full Stack Dev</h3> <h4>Hobbies: Outdoors</h4>")
+})
+
+app.get("/hobbies", function(reeq,res){
+    res.send("<h1>Coding</h1> <h1>Outdoors</h1> <h1>Beer</h1> <h1>Family</h1>")
+})
+
+app.listen(3000, function () {
     console.log('Server started on port 3000');
 });
