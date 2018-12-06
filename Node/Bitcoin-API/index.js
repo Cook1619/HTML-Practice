3bdc1218a6911e05c4ab1ee185b28ce62c8e2c1e
Matt <cook6348@gmail.com>
2018-12-06 06:10:36 -0600

    Bitcoin post route now working

--- Node/Bitcoin-API/index.js
+++ Node/Bitcoin-API/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const axios = require('axios')
 
 const app = express();
 
@@ -12,11 +13,23 @@ app.get("/", function(req,res){
 
 app.post("/", function(req,res){
     request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", function(error, res, body){
+        //Add error message 
         let data = JSON.parse(body);
-        let price = data.averages.week;
-        res.send(`The price of bitcoin is ${price}`)
+        let price = data.last;
+        res.send("<h1>The current price of bitcoin is " + price + " !<h1>")
     })
 })
+
+// app.post("/", async function(req,res){
+//     try {
+//         const { data } = await axios("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD")
+//         //Add error message 
+//         let price = data.averages.week;
+//         res.send(`The price of bitcoin is ${price}`)
+//     } catch (err) {
+//         res.send(404)
+//     }
+// })
 
 app.listen(3000, function(){
     console.log('Server running on port 3000');
