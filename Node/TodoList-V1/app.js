5fd7563eaf8bf8bbe9df5c9af79364402b54a0a6
Matt <cook6348@gmail.com>
2018-12-08 22:12:25 -0600

    tets

--- Node/TodoList-V1/app.js
+++ Node/TodoList-V1/app.js
@@ -1,19 +1,23 @@
 //jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res){
+app.use("view engine", "ejs");
+
+app.get("/", function (req, res) {
   let today = new Date();
-  if(today.getDay() === 6 || today.getDay() === 0 ){
-    res.send("Its the weekend!")
-  }else {
-    res.sendFile(__dirname + "/index.html")
+  let day = ""
+  if (today.getDay() === 6 || today.getDay() === 0) {
+    day = "Weekend";
+  } else {
+    day = "Weekday";
   }
+  res.render("list", { kindOfDay: day });
 });
 
-app.listen(3000, function(){
+console.log("test")
+app.listen(3000, function () {
   console.log("Server started on port 3000.");
 });
