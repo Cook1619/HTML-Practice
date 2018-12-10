76711fadb4ef2926471d368e5e31024a1984932c
Matt <cook6348@gmail.com>
2018-12-10 07:56:53 -0600

    adding post logic for todo list

--- Node/TodoList-V1/app.js
+++ Node/TodoList-V1/app.js
@@ -4,41 +4,27 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
 
   let today = new Date();
-  let currentDay = today.getDay();
-  let day = "";
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wenesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error: current day is equal to: " + currentDay);
+  let options = {
+    weekday: 'long',
+    day: 'numeric',
+    month:'long'
   }
+  let day = today.toLocaleDateString("en-US", options);
+
   res.render("list", { kindOfDay: day });
 });
+
+app.post("/", function(req,res){
+  console.log(req.body.newItem);
+})
 
 console.log("test")
 app.listen(3000, function () {
