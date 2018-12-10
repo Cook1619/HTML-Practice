a3439e2b8dc3353f25c5985bccf8d3ce081e7d42
Matt <cook6348@gmail.com>
2018-12-10 12:36:20 -0600

    Added about page using partials

--- Node/TodoList-V1/app.js
+++ Node/TodoList-V1/app.js
@@ -49,7 +49,10 @@ app.post("/work", function (req, res) {
   res.redirect("/work");
 })
 
-console.log("test")
+app.get("/about", function(req,res){
+  res.render("about");
+})
+
 app.listen(3000, function () {
   console.log("Server started on port 3000.");
 });
