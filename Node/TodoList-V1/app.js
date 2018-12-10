46a1c3819900ae615001790a8dc7c1e1ca246375
Matt <cook6348@gmail.com>
2018-12-10 06:42:48 -0600

    Added all case statements

--- Node/TodoList-V1/app.js
+++ Node/TodoList-V1/app.js
@@ -7,12 +7,35 @@ const app = express();
 app.use("view engine", "ejs");
 
 app.get("/", function (req, res) {
+
   let today = new Date();
-  let day = ""
-  if (today.getDay() === 6 || today.getDay() === 0) {
-    day = "Weekend";
-  } else {
-    day = "Weekday";
+  let currentDay = today.getDay();
+  let day = "";
+
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wenesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log("Error: current day is equal to: " + currentDay);
   }
   res.render("list", { kindOfDay: day });
 });
