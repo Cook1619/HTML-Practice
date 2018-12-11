bb790d83e0bccbc0854e2664708c070bd7921004
Matt <cook6348@gmail.com>
2018-12-11 05:44:36 -0600

    brought that date functionality into another file so it more reusable, refactored code, commented code

--- Node/TodoList-V1/app.js
+++ Node/TodoList-V1/app.js
@@ -1,34 +1,29 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js")
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
-
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
+//Allows use to extract data from our html form
 app.use(bodyParser.urlencoded({ extended: true }));
+//Allows static files in the public folder to be served up
 app.use(express.static("public"));
-
+//Sets up EJS to be used
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-
-  let today = new Date();
-
-  let options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long'
-  }
-  let day = today.toLocaleDateString("en-US", options);
+  //This is a function in our date.js which we are requiring at the top of the file, and assigning to the variable day
+  const day = date.getDate();
   //we render out list.ejs which first renders the day, then renders the newListItems
   res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
   //This is the todo we typed into the input, and were saving it to a variable to render in the above app.get
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   if (req.body.list === "Work") {
     workItems.push(item);
     res.redirect("/work");
@@ -44,12 +39,12 @@ app.get("/work", function (req, res) {
 });
 
 app.post("/work", function (req, res) {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   workItems.push(item);
   res.redirect("/work");
 })
 
-app.get("/about", function(req,res){
+app.get("/about", function (req, res) {
   res.render("about");
 })
 
