86f0db4a400addcddf3e94b22614847fbc606412
Matt <cook6348@gmail.com>
2018-12-19 08:20:11 -0600

    Setting up data to put in Mongo db

--- Node/FruitsProject/app.js
+++ Node/FruitsProject/app.js
@@ -8,14 +8,47 @@ const url = 'mongodb://localhost:27017';
 const dbName = 'fruitsDB';
 
 // Create a new MongoClient
-const client = new MongoClient(url);
+const client = new MongoClient(url, { useNewUrlParser: true });
 
 // Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+client.connect(function (err) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
 
-  const db = client.db(dbName);
+    const db = client.db(dbName);
 
-  client.close();
+    insertDocuments(db, function() {
+        client.close();
+      });
 });
+
+const insertDocuments = function (db, callback) {
+    // Get the documents collection
+    const collection = db.collection('fruits');
+    // Insert some documents
+    collection.insertMany([
+        {
+            name: "Apple",
+            score: 10,
+            review: "Amazing!"
+        },
+        {
+            name: "Orange",
+            score: 7,
+            review: "Great but messy"
+        },
+        {
+            name: "Banana",
+            score: 10,
+            review: "Very versitile"
+        }
+    ], function (err, result) {
+        //validate to make sure there are no errors in doc
+        assert.equal(err, null);
+        //Makes sure were inserting 3 results into the document
+        assert.equal(3, result.result.n);
+        assert.equal(3, result.ops.length);
+        console.log("Inserted 3 documents into the collection");
+        callback(result);
+    });
+}
