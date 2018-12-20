eaf8a756038c6c268e64f0e85bc00503b1575574
Matt <cook6348@gmail.com>
2018-12-20 07:45:35 -0600

    Added validation to some fields

--- Node/FruitsProject/app.js
+++ Node/FruitsProject/app.js
@@ -3,36 +3,51 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/fruits', {useNewUrlParser: true});
 
 const fruitSchema =  new mongoose.Schema ({
-    name: String,
-    rating: Number,
+    name: {
+        //Makes the name field required to add to database
+        type: String,
+        required: [true, "Please add a name to your entry"]
+    },
+    //this creates validation which a rating has to be between 1-10
+    rating: {
+        type: Number,
+        min: 1,
+        max: 10
+    },
     review: String
 });
 
 const Fruit =  mongoose.model("Fruit", fruitSchema);
 
 const fruit = new Fruit ({
-    name: "Apple",
-    rating: 8,
-    review: "Good fruit!"
+    name: "Peach",
+    rating: 10,
+    review: "This is a review"
 })
+fruit.save();
+// const fruit = new Fruit ({
+//     name: "Apple",
+//     rating: 8,
+//     review: "Good fruit!"
+// })
 
-const orange = new Fruit({
-    name: "Orange",
-    rating: 7,
-    review: "Good enough"
-})
+// const orange = new Fruit({
+//     name: "Orange",
+//     rating: 7,
+//     review: "Good enough"
+// })
 
-const strawberry =  new Fruit ({
-    name: "Strawberry",
-    rating: 9,
-    review: "The best berry"
-})
+// const strawberry =  new Fruit ({
+//     name: "Strawberry",
+//     rating: 9,
+//     review: "The best berry"
+// })
 
-const blueberry =  new Fruit ({
-    name: "Blueberry",
-    rating: 8.5,
-    review: "Pretty darn good"
-})
+// const blueberry =  new Fruit ({
+//     name: "Blueberry",
+//     rating: 8.5,
+//     review: "Pretty darn good"
+// })
 //Inserts all these fruits every time the program is ran
 // Fruit.insertMany([orange,strawberry,blueberry], function(err){
 //     if(err){
