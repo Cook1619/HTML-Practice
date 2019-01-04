ec9b6fe5a6faed75a0e6f1b7b2d621e29d93516b
Matt <cook6348@gmail.com>
2019-01-04 08:04:54 -0600

    notes added

--- Projects/toDoList-V2/app.js
+++ Projects/toDoList-V2/app.js
@@ -2,22 +2,22 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-
-mongoose.connect("mongodb://locahost:27017/todolistDB", {useNewUrlParser: true});
-
+//mongoose connecting to mongoDB and creating todolistDB
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+//New Schema added
 const itemsSchema = {
-  name: String
+  item: String
 };
-
+//Mongoose model based on the itemsSchema
 const Item = mongoose.model("Item", itemsSchema );
-
+//Static data added to ToDoList
 const item1 = new Item({
   item: "Welcome to your todoList!"
 });
@@ -29,15 +29,15 @@ const item3 = new Item({
 });
 
 const defaultItems = [item1,item2,item3]
-
+//Refers to the Item model, and insertMany is a built in mongoose model method, and the 2 args are the items we wanna put in the db and an error is there is one
 Item.insertMany(defaultItems, function(err){
   if(err){
     console.log(err);
   }else{
-    console.log("Sucess!")
+    console.log("Success!")
   }
 })
-
+// Item.remove({},function(err){console.log('removed')})
 app.get("/", function(req, res) {
 
   res.render("list", {listTitle: "Today", newListItems: items});
